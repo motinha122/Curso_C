4491fc50b4b8830eac533afb9ac5018e0e9c587f
Tiago Mota <timota132@gmail.com>
2022-03-24 20:03:56 -0300

    Arrays as arguments

--- file1.c
+++ file1.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
-#include <stdbool.h>
 
-void doublevalue(int *x)
+void doubleElements(int a[],int size)
 {
-    *x *=2;
+    for (int i =0; i < size ;i++)
+    {
+        a[i] *=2;
+    }
 }
 
 int main() 
 {
-    int x;
-    printf("Value to double: ");
-    scanf("%d",&x);
-    doublevalue(&x);
-    printf("Double value is: %d",x);
+    int A[] = {1,2,3,4,5};
+    int size = sizeof(A)/sizeof(A[0]);
+    doubleElements(A,size);
+    for (int i =0;i < size;i++)
+    {
+        printf("%d ",A[i]);
+        printf("%d\n", &A+i);
+    }
     return 0;
 }
 

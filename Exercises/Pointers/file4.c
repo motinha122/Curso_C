22c70a1334bf4b17e842b06c3600caeabef3f716
Tiago Mota <timota132@gmail.com>
2022-03-29 20:24:08 -0300

    2d Array pointers

---
+++ Exercises/Pointers/file4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void printArray(int* A,int rowSize, int colSize)
+{
+    for(int row = 0; row < rowSize; row++)
+    {
+        for(int col = 0; col < colSize; col++)
+        {
+            printf("Address of B[%d][%d] is : %d \n",row,col,(A+row*colSize)+col);
+            printf("Value of B[%d][%d] is : %d \n\n",row,col,*((A+row*colSize)+col));
+        }
+    }
+}
+
+int main() 
+{
+                                                    //                   [Example] Address :    400,404 ,408, 412, 416, 420
+    int B[2][3] = {{4,5,6},{7,8,9}};    // This 2D array its a 1D array         |  4  |  5  |  6  |  7  |  8  |  9  | 
+                                                    // with 3 ints per row (2r and 3c)           --B[0]-- | --B[1]--               
+    int size = sizeof(B)/sizeof(B[0][0]); //sizeof => size in bytes
+
+    printArray((int *)B,2,3);
+
+    return 0;
+}
+
